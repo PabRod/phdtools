48136103d37a3cd93d7eb0a1cabeb0a1c6863c2d
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2019-03-28 09:25:30 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/PabRod/toypy
+
+from setuptools import setup, find_packages
+
+
+with open('readme.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='phdtools',
+    version='0.1.0',
+    description='Toy package',
+    long_description=readme,
+    author='Pablo Rodríguez-Sánchez',
+    author_email='pablo.rodriguez.sanchez@gmail.com',
+    url='https://github.com/PabRod/phdtools',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
